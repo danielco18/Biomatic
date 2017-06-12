7e25536775e9f1f49eb493f938fd32d290ebc362
d <d>
2017-06-12 11:35:34 -0500

    dd

--- views/assets/js/main.js
+++ views/assets/js/main.js
@@ -1,48 +1,8 @@
-// $("#emailSignUp").focusout(function(){
-//     $("#emailSignUp").siblings("small").remove();
-//     var email = "prueba@gmail.com";
-//     if (email.length > 0) {
-//         $.post("index.php?c=user&a=validEmailSignUp",{email:email},function(response){            var response = JSON.parse(response);
-//             $("#emailSignUp").after("<small id='emailHelp' class='form-text text-muted'>"+response[0]+"</small>")
-//             if (response[1] == false) {
-//                 $("#butonSignUp").attr("disabled",true);
-//             } else {
-//                 $("#butonSignUp").attr("disabled",false);
-//             }
-//         });
-//     }
-// })
+$('[data-toggle="tooltip"]').tooltip()
 
-// $(".email").focus(function(){
-//    $(this).siblings("small").remove();
-//    $(".form-button").attr("disabled",false);
-// })
-
-// $("#frmSignUp").submit(function(e){
-//     e.preventDefault();
-//     var data =  [
-//                     $("#name").val(),
-//                     $("#lastName").val(),
-//                     $("#emailSignUp").val(),
-//                     $("#passSignUp").val(),
-//                     $("#passSignUpR").val()
-//                 ];
-//     $.post("index.php?c=user&a=create",{data:data},function(response){
-//         var response = JSON.parse(response);
-//         if (response[0] == true) {
-//             alert("Datos ingresados correctamente");
-//             document.location.href = response[1];
-//         } else {
-//             alert(response[1]);
-//         }
-//     })
-// })
-
- $('[data-toggle="tooltip"]').tooltip()
-
- $('#menu-icon').click(function(){
+$('#menu-icon').click(function(){
     $('.menu-responsive-container').toggleClass('hidden')
- })
+});
 
 $.extend( true, $.fn.dataTable.defaults, {
     "ordering": true,
@@ -64,3 +24,19 @@ $('#dataTable').DataTable({
         }
     }
 });
+
+$('#emailSignUp').focusout(function(){
+    $('#emailSignUp').siblings("small").remove()
+    var data = $('#emailSignUp').val()
+    if (data.length > 0) {
+        $.post("index.php?c=user&a=validEmailSignUp",{data:data},function(response){
+            var response = JSON.parse(response)
+            $('#emailSignUp').after("<small>"+response[0]+"</small>")
+            if (response[1] == false) {
+                $('#butonSignUp').attr("disabled",true)
+            } else {
+                $('#butonSignUp').attr("disabled",false)
+            }
+        })
+    }
+})
